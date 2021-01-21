4e4a99d3232b4ac8d2ba34d10a54643cc18cbbc9
Renan Aquino <renan.dowglasaquino@hotmail.com>
2021-01-21 00:55:04 -0300

    Alteracoes Renan

--- front/urls.py
+++ front/urls.py
@@ -4,5 +4,7 @@ from front import views as views_front
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views_front.init, name='init'),
+    path('', views_front.index, name='index'),
+    path('consulta/', views_front.consulta, name='consulta'),
+
 ]
--- front/views.py
+++ front/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
 
-def init(request):
+def index(request):
+	return render(request, "index.html")
+
+def consulta(request):
 	return render(request, "consulta.html")
+
